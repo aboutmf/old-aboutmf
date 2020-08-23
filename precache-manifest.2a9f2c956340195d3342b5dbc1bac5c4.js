3278b42fcad2aeb85253fbd083b13820845bcdf6
aboutmf <mf.about@gmail.com>
2020-08-23 12:39:08 +0700

    Updates

---
+++ precache-manifest.2a9f2c956340195d3342b5dbc1bac5c4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b0a525139b9b88f76566bc0baf0f245",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19265bba1c347f6e6df9",
+    "url": "/static/css/2.5646f6cd.chunk.css"
+  },
+  {
+    "revision": "58ed94e79b5fe10d7345",
+    "url": "/static/css/main.6cfc1e3e.chunk.css"
+  },
+  {
+    "revision": "19265bba1c347f6e6df9",
+    "url": "/static/js/2.505eeba9.chunk.js"
+  },
+  {
+    "revision": "3fe96f3e6594cc50b4fb27fd0dc3f48f",
+    "url": "/static/js/2.505eeba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58ed94e79b5fe10d7345",
+    "url": "/static/js/main.0c0f3798.chunk.js"
+  },
+  {
+    "revision": "5820d58c60ab3b1375ad",
+    "url": "/static/js/runtime-main.99b514f4.js"
+  },
+  {
+    "revision": "02d93ae14cd1f9f124ce68c6014468fb",
+    "url": "/static/media/3d.02d93ae1.svg"
+  },
+  {
+    "revision": "51cb14f8bc6c705e8b6dd782275d7e81",
+    "url": "/static/media/83_prominent.51cb14f8.jpg"
+  },
+  {
+    "revision": "23f2b7ad806b792180594654f2e646ed",
+    "url": "/static/media/laravel.23f2b7ad.png"
+  }
+]);
